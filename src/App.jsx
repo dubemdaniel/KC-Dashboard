b7291a3d818ec5674188ed665cf05ebc6eb2a92c
Daniel Chukwudubem <princedaniel7890@gmail.com>
2024-06-28 10:44:55 +0100

    downloaded icons for signUp page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Apple from './assets/images/Apple.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>ur papa</div>
+     <img src={Apple} alt="" />
+    </>
+  )
+}
+
+export default App
