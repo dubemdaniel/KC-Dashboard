79991f869a248fc2047ac8a8eeeb936077f977de
Daniel Chukwudubem <princedaniel7890@gmail.com>
2024-06-29 15:03:10 +0100

    added all login and sighUp pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
-import Apple from './assets/images/Apple.svg'
+// import Apple from './assets/images/Apple.svg'
+import SignUp from './components/registration/SignUp'
+import PersonalInformation from './components/registration/PersonalInfo'
+import AddAddress from './components/registration/AddAddress'
+import ManualAddress from './components/registration/ManualAddress'
+import Login from './components/registration/Login'
 
 function App() {
 
   return (
     <>
-      <div>ur papa</div>
-     <img src={Apple} alt="" />
+      <div className='gap-16 flex flex-col'>
+      <SignUp />
+      <PersonalInformation />
+        <AddAddress />
+        <ManualAddress />
+        <Login/>
+      </div>
     </>
   )
 }
