424632afa093843f973d9a578d9979a0ec7ef712
Daniel Chukwudubem <princedaniel7890@gmail.com>
2024-07-02 09:34:10 +0100

    sidebar 80% done

--- src/components/dashboard/SideBar.jsx
+++ src/components/dashboard/SideBar.jsx
@@ -1,84 +1,158 @@
 import React, { useState } from "react";
-import People from "../../assets/images/People.svg";
-import '../../index.css';
+// import People from "../../assets/images/People.svg";
+import "../../index.css";
+import Dashboardmini from "../../assets/SideImages/Dashboardmini";
+import ProductLogo from "../../assets/SideImages/ProductLogo";
+import CustomersLogo from "../../assets/SideImages/CustomersLogo";
+import IncomeLogo from "../../assets/SideImages/IncomeLogo";
+import PromoteLogo from "../../assets/SideImages/PromoteLogo";
+import HelpLogo from "../../assets/SideImages/HelpLogo";
+import DashboardLogo from "../../assets/SideImages/DashboardLogo";
 
-const Bar = () => {
+// const Bar = () => {
 //   const [isOpen, setIsOpen] = useState(false);
 
 //   const toggleSidebar = () => {
 //     setIsOpen(!isOpen);
 //   };
-    
-return (
-    <div className="flex flex-col h-screen bg-[white] text-black w-16 hover:w-64 transition-width duration-300  group hover:items-start">
-      <ul className="flex flex-col items-start  p-4 space-y-4 group-hover:items-start">
-        <li className="flex items-start space-x-4 ">
-          <img src={People} />
-          <span className="hidden group-hover:inline-block opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">Home</span>
-        </li>
-        <li className="group flex items-start space-x-4">
-        <img src={People} />
-          <span className="hidden group-hover:inline-block">Profile</span>
-        </li>
-        <li className="group flex items-start space-x-4">
-        <img src={People} />
-          <span className=" hidden group-hover:inline-block">Settings</span>
-        </li>
+// const [isHovered, setIsHovered] = useState(false);
+
+const Bar = () => {
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  const svgIcons = [
+    {
+      component: (
+        <Dashboardmini
+          fill={hoveredIcon === "Dashboard" ? "white" : "#9197B3"}
+        />
+      ),
+      name: "Dashboard",
+    },
+    {
+      component: (
+        <ProductLogo fill={hoveredIcon === "Product" ? "white" : "#9197B3"} />
+      ),
+      name: "Product",
+    },
+    {
+      component: (
+        <CustomersLogo
+          fill={hoveredIcon === "Customers" ? "white" : "#9197B3"}
+        />
+      ),
+      name: "Customers",
+    },
+    {
+      component: (
+        <IncomeLogo fill={hoveredIcon === "Income" ? "white" : "#9197B3"} />
+      ),
+      name: "Income",
+    },
+    {
+      component: (
+        <PromoteLogo fill={hoveredIcon === "Promote" ? "white" : "#9197B3"} />
+      ),
+      name: "Promote",
+    },
+    {
+      component: (
+        <HelpLogo fill={hoveredIcon === "Help" ? "white" : "#9197B3"} />
+      ),
+      name: "Help",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col h-screen bg-[white] text-black w-16 hover:w-64 transition-width duration-300 group hover:items-start">
+      <div className=" gap-2 p-5 flex align-middle mt-6 mb-4">
+        <div>
+          {" "}
+          <DashboardLogo />
+        </div>
+        <span className=" hidden group-hover:inline-block text-2xl font-medium">
+          Dashboard <span className="text-[10px] text-[#9197B3] opacity-60 ">v.01</span>
+        </span>
+      </div>
+
+      <ul className="flex flex-col items-start p-5 gap-4 space-y-4 group-hover:items-start w-[100%]">
+        {svgIcons.map((icon, index) => (
+          <li
+            key={index}
+            className="flex items-start space-x-4 w-[100%] hover:bg-[#5932EA] hover:p-2 hover:rounded-lg hover:text-white"
+            onMouseEnter={() => setHoveredIcon(icon.name)}
+            onMouseLeave={() => setHoveredIcon(null)}
+          >
+            {icon.component}
+            <span className="hidden group-hover:inline-block ">{icon.name}</span>
+          </li>
+        ))}
       </ul>
     </div>
   );
-  
-  
-    // return (
-    //     <div className="sidebar h-screen bg-blue-800 text-white w-16 hover:w-64 transition-width duration-300">
-    //     <ul className="flex flex-col items-center hover:items-start p-4 space-y-4">
-    //       <li className="flex items-center space-x-4">
-    //         <img src={People} />
-    //         <span className="sidebar-text hidden hover:block">Home</span>
-    //       </li>
-    //       <li className="flex items-center space-x-4">
-    //       <img src={People} />
-    //         <span className="sidebar-text hidden hover:inline">Profile</span>
-    //       </li>
-    //       <li className="flex items-center space-x-4">
-    //       <img src={People} />
-    //         <span className="sidebar-text hidden hover:inline">Settings</span>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // );
-  
-  
-    
-  
 
-//   return (
-//     <div className="flex align-middle text-center items-center w-screen relative">
-//       <button
-//         onClick={toggleSidebar}
-//         className="p-2 bg-blue-500 text-white items-center absolute right-5 top-5"
-//       >
-//         Toggle Sidebar
-//       </button>
-//       <div
-//         className={`fixed inset-y-0 left-0 w-64 bg-gray-800 text-white transform ${
-//           isOpen ? "translate-x-0" : "-translate-x-full"
-//         } transition-transform duration-300 ease-in-out`}
-//       >
-//         <ul>
-//           <li className="p-4 border-b border-gray-700 flex align-middle gap-4">
-//             <span>
-//                           <img src={People} alt="" className="text-white" />
-                          
-//             </span>{" "}
-//             Home
-//           </li>
-//           <li className="p-4 border-b border-gray-700">About</li>
-//           <li className="p-4 border-b border-gray-700">Contact</li>
-//         </ul>
-//       </div>
-//     </div>
-//   );
+  // <li className="flex items-start space-x-4 w-[100%] " onMouseEnter={() => setIsHovered(true)}
+  // onMouseLeave={() => setIsHovered(false)}>
+  //      <Dashboardmini fill={isHovered ? 'blue' : 'red'} />
+  //       <span className="hidden group-hover:inline-block">Dashboard</span>
+  //     </li>
+  //     <li className="group flex items-start space-x-4">
+  //     <ProductLogo fill={isHovered ? 'white' : '#9197B3'}/>
+  //       <span className="hidden group-hover:inline-block">Profile</span>
+  //     </li>
+  //     <li className="group flex items-start space-x-4">
+  //     <CustomersLogo fill={isHovered ? 'white' : "#9197B3"}/>
+  //       <span className=" hidden group-hover:inline-block">Settings</span>
+  //     </li>
+
+  // return (
+  //     <div className="sidebar h-screen bg-blue-800 text-white w-16 hover:w-64 transition-width duration-300">
+  //     <ul className="flex flex-col items-center hover:items-start p-4 space-y-4">
+  //       <li className="flex items-center space-x-4">
+  //         <img src={People} />
+  //         <span className="sidebar-text hidden hover:block">Home</span>
+  //       </li>
+  //       <li className="flex items-center space-x-4">
+  //       <img src={People} />
+  //         <span className="sidebar-text hidden hover:inline">Profile</span>
+  //       </li>
+  //       <li className="flex items-center space-x-4">
+  //       <img src={People} />
+  //         <span className="sidebar-text hidden hover:inline">Settings</span>
+  //       </li>
+  //     </ul>
+  //   </div>
+  // );
+
+  // opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out
+
+  //   return (
+  //     <div className="flex align-middle text-center items-center w-screen relative">
+  //       <button
+  //         onClick={toggleSidebar}
+  //         className="p-2 bg-blue-500 text-white items-center absolute right-5 top-5"
+  //       >
+  //         Toggle Sidebar
+  //       </button>
+  //       <div
+  //         className={`fixed inset-y-0 left-0 w-64 bg-gray-800 text-white transform ${
+  //           isOpen ? "translate-x-0" : "-translate-x-full"
+  //         } transition-transform duration-300 ease-in-out`}
+  //       >
+  //         <ul>
+  //           <li className="p-4 border-b border-gray-700 flex align-middle gap-4">
+  //             <span>
+  //                           <img src={People} alt="" className="text-white" />
+
+  //             </span>{" "}
+  //             Home
+  //           </li>
+  //           <li className="p-4 border-b border-gray-700">About</li>
+  //           <li className="p-4 border-b border-gray-700">Contact</li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   );
 };
 
 export default Bar;
